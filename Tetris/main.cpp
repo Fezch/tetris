9890620f9cffb2f4399b5efee0db8858a116146c
Andrew <andrewf1993@gmail.com>
2015-09-02 20:36:13 +1200

    Added Timer
    Added a simple timer refresh.

--- Tetris/main.cpp
+++ Tetris/main.cpp
@@ -4,6 +4,7 @@
 #include <sstream>
 #include "bits.h"
 #include <windows.h>
+#include <time.h>
 
 //Namespace
 using namespace std;
@@ -21,14 +22,8 @@ string drawBoard()
 		{
 			//Build String
 			boardBuilder << board[y][x] << " ";
-			
-			//Legacy cout
-			//cout << board[y][x] << " ";
 		}
 		boardBuilder << "\n";
-
-		//Legacy cout
-		//cout << endl;
 	}
 	return boardBuilder.str();
 }
@@ -53,17 +48,28 @@ int main()
 	//Boolean for game over
 	bool gameOver = false;
 
+	//Doubles for timing
+	double timerStart = (clock() / CLOCKS_PER_SEC);
+	double timerCurrent = timerStart;
+
 	//Main loop
 	while (!gameOver)
 	{
-		//Clear Screen
-		system("CLS");
+		//Save current time
+		timerCurrent = (clock() / CLOCKS_PER_SEC);
 
-		//Draw Board
-		cout << drawBoard();
+		//Check if screen needs refreshed
+		if (timerCurrent > timerStart + 0.5)
+		{
+			//Clear screen
+			system("CLS");
 
-		//Wait for a second
-		Sleep(100);
+			//Draw Board
+			cout << drawBoard();
+
+			//Reset timerCurrent
+			timerStart = (clock() / CLOCKS_PER_SEC);
+		}
 	}
 
 	return 0;
